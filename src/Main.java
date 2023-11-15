c4ecdf8b20b967d1a4369cca67eba3b2b1c45874
g21turbo <dg21stew@gmail.com>
2023-11-14 19:49:28 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, double extraBuckets) {
+
+        return 1;
+    }
+
+}
