67f74106bb68c216231904a8d1a0f115e62bf9aa
g21turbo <dg21stew@gmail.com>
2023-11-16 23:17:40 -0800

    overloaded getBucketCount complete

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ public class Main {
         System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
         System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
 
+        System.out.println(getBucketCount(-3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(3.4, 2.1, 1.5));
+        System.out.println(getBucketCount(7.25, 4.3, 2.35));
+
     }
 
     public static int getBucketCount(double width, double height, double areaPerBucket, double extraBuckets) {
@@ -16,6 +20,18 @@ public class Main {
 
         double area = width * height;
         double bucketCount = area / areaPerBucket - extraBuckets;
+
+        return (int) Math.round(bucketCount);
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+
+        double area = width * height;
+        double bucketCount = area / areaPerBucket;
 
         return (int) Math.round(bucketCount);
     }
