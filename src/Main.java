b7409154a5f45b64121595c944784722ff2d85ba
g21turbo <dg21stew@gmail.com>
2023-11-16 22:51:21 -0800

    input validation added

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,13 @@ public class Main {
 
     public static int getBucketCount(double width, double height, double areaPerBucket, double extraBuckets) {
 
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+
+        double area = width * height;
+
+
         return 1;
     }
 
