6d15ca5ee822709a16d1e96dac94dd858dc3ddfa
g21turbo <dg21stew@gmail.com>
2023-11-19 17:29:22 -0800

    finished product, changed Math.round to Math.ceil

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,22 @@ public class Main {
         System.out.println(getBucketCount(3.4, 2.1, 1.5, 2));
         System.out.println(getBucketCount(2.75, 3.25, 2.5, 1));
 
+        System.out.println();
+
         System.out.println(getBucketCount(-3.4, 2.1, 1.5));
         System.out.println(getBucketCount(3.4, 2.1, 1.5));
         System.out.println(getBucketCount(7.25, 4.3, 2.35));
 
+        System.out.println();
+
+        System.out.println(getBucketCount(3.4, 1.5));
+        System.out.println(getBucketCount(6.26, 2.2));
+        System.out.println(getBucketCount(3.26, .75));
+
+
     }
 
-    public static int getBucketCount(double width, double height, double areaPerBucket, double extraBuckets) {
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
 
         if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
             return -1;
@@ -21,7 +30,7 @@ public class Main {
         double area = width * height;
         double bucketCount = area / areaPerBucket - extraBuckets;
 
-        return (int) Math.round(bucketCount);
+        return (int) Math.ceil(bucketCount);
     }
 
     public static int getBucketCount(double width, double height, double areaPerBucket) {
@@ -33,7 +42,17 @@ public class Main {
         double area = width * height;
         double bucketCount = area / areaPerBucket;
 
-        return (int) Math.round(bucketCount);
+        return (int) Math.ceil(bucketCount);
     }
 
+    public static int getBucketCount(double area, double areaPerBucket) {
+
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+
+        double bucketCount = area / areaPerBucket;
+
+        return (int) Math.ceil(bucketCount);
+    }
 }
